e6fe33ad7dc7751f5ed3a42a4068893db66d1e0b
Abdraloguzov100 <Abdraloguzov100@outlook.com>
2022-05-30 15:28:15 +0500

    Добавьте файлы проекта.

---
+++ BBS/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.IO;
+
+namespace BBS
+{
+    class Program
+    {
+        static BigInteger[] GetIntFromFile(string filepath) {
+            var fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                BigInteger p = BigInteger.Parse(streamReader.ReadLine());
+                BigInteger q = BigInteger.Parse(streamReader.ReadLine());
+                return new BigInteger[] {p, q };
+            }
+            throw new Exception("ФАЙЛ НЕ ПРОСЧИИАТН");
+           
+        }
+
+        static byte[] GetSeriesFromFile(string filepath)
+        {
+            var fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            List<byte> series = new List<byte>();
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    series.Add(byte.Parse(streamReader.ReadLine()));
+                }
+                return series.ToArray();
+            }
+            
+            throw new Exception("ФАЙЛ НЕ ПРОСЧИИАТН");
+           
+        }
+
+        static void showArray<T>(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+                Console.Write(", ");
+            }
+        }
+
+        static void PutIntFromFile(string filepath, byte[] seqs)
+        {
+            var fileStream = new FileStream(filepath, FileMode.OpenOrCreate, FileAccess.Write);
+            using (var streamWriter = new StreamWriter(fileStream, Encoding.UTF8))
+            {
+                foreach(var el in seqs ) {
+                    streamWriter.WriteLine(el.ToString());
+                }
+
+                return;
+            }
+            throw new Exception("ФАЙЛ НЕ ПРОСЧИИАТН");
+
+        }
+
+        static BigInteger GCD(BigInteger x, BigInteger y)
+        {
+            while (y != 0)
+            {
+                var t = y;
+                y = x % y;
+                x = t;
+            }
+            return x;
+        }
+
+
+        static byte[] BBS(BigInteger p, BigInteger q, int N)
+        {
+            if (p % 4 != 3 || q % 4 != 3)
+                throw new Exception("Выбранный простые числа не подходят");
+            BigInteger M = p * q;
+            Random random = new Random();
+            byte[] series = new byte[N];
+            int x = random.Next(1000,int.MaxValue);
+            while (GCD(x, M) != 1)
+            {
+                x = random.Next(int.MaxValue);
+            }
+            BigInteger x1 = BigInteger.ModPow(x, 2, M);
+            for (int i = 0; i < N; i++)
+            {
+                series[i] = (byte) (x1 % 2);
+                x1 = BigInteger.ModPow(x1, 2, M);
+            }
+            return series;
+        }
+
+
+
+        static bool freq_test(string filename, bool show = true)
+        {
+            byte[] series = GetSeriesFromFile(filename);
+            int sum = 0;
+            for (int i = 0; i < series.Length; i++)
+            {
+                sum += 2 * series[i] - 1;
+            }
+            double s = Math.Abs(sum) / Math.Sqrt(series.Length);
+            if (show)
+            {
+                
+                Console.Write("\ns = ");
+                Console.Write(s);
+                Console.WriteLine();
+                if(s <= 1.82138636) {
+                    Console.WriteLine("Последовательность прошла проверку частотности");
+                }
+                else Console.WriteLine("Последовательность не прошла проверку частотности");
+
+            }
+            return s <= 1.82138636;
+        }
+
+        static bool subseries_test(string filename, bool show = true)
+        {
+            byte[] series = GetSeriesFromFile(filename);
+            int piSum = 0;
+            for (int i = 0; i < series.Length; i++)
+            {
+                piSum += series[i] ;
+            }
+            double piS = (piSum+0.0) / series.Length;
+            int Vn = 1;
+            for (int i = 1; i < series.Length-1; i++)
+            {
+                Vn += series[i]==series[i+1] ? 1: 0;
+            }
+            double s = Math.Abs(Vn - 2 * piS * series.Length * (1 - piS)) / 
+                (2 * Math.Sqrt(2 * series.Length) * piS * (1 - piS));
+            if (show)
+            {
+                
+                Console.Write("\ns = ");
+                Console.Write(s);
+                Console.WriteLine();
+                if (s <= 1.82138636)
+                {
+                    Console.WriteLine("Последовательность прошла проверку на подпоследовательность");
+                }
+                else Console.WriteLine("Последовательность не прошла проверку на подпоследовательность");
+
+            }
+            return s <= 1.82138636;
+        }
+
+
+        static bool dispersion_test(string filename, bool show = true)
+        {
+            
+            byte[] series = GetSeriesFromFile(filename);
+            int[] Ss = new int[series.Length + 2];
+            Ss[0] = 0;
+            for (int i = 0; i < series.Length; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    Ss[i + 1] += 2 * series[j] - 1;
+                }
+            }
+            int k = 0;
+            for(int i=0; i < Ss.Length; i++)
+            {
+                if (Ss[i] == 0) k++;
+            }
+            int L = k - 1;
+            int[] ksij = new int[19];
+            double[] Ys = new double[19]; 
+            for(int j =-9; j <= 9; j++)
+            {
+                if (j == 0) continue;
+                for (int i = 0; i < Ss.Length; i++)
+                {
+                    if (Ss[i] == j) ksij[j+9]++;
+                }
+                Ys[j + 9] = Math.Abs(ksij[j + 9] - L) / 
+                    Math.Sqrt(2 * L * (4 * Math.Abs(j) - 2));
+            }
+            bool result = true;
+            for (int i = 0; i < Ys.Length; i++)
+            {
+                if (Ys[i] > 1.82138636) result = false;
+            }
+            if (show)
+            {
+                Console.Write("\nS' = ");
+                showArray(Ss);
+                Console.Write("\nL = ");
+                Console.Write(L);
+                Console.Write("\nksij = ");
+                showArray(ksij);
+                Console.Write("\nYj = ");
+                showArray(Ys);
+                Console.WriteLine();
+                
+                if (result)
+                {
+                    Console.WriteLine("Последовательность прошла проверку на  произвольные отклонения");
+                }
+                else Console.WriteLine("Последовательность не прошла проверку на  произвольные отклонения");
+
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string fileOut = @"I:\BBS\BBS\OUT.txt";
+            BigInteger[] pq = GetIntFromFile(@"I:\BBS\BBS\IN.txt");
+            byte[] series = BBS(pq[0], pq[1], 200);
+            //PutIntFromFile(fileOut, series);
+            
+            byte[] test = GetSeriesFromFile(fileOut);
+            showArray(test);
+            freq_test(fileOut);
+            subseries_test(fileOut);
+            dispersion_test(fileOut);
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
